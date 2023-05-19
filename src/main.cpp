309f2017fecee0ff875df2267e8d6d8037e19e75
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-05-19 10:25:40 +0200

    Add support for regular old arduino uno

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,50 @@
 #include <Arduino.h>
+
+#ifdef IS_PRO_MICRO
 #include <MIDIUSB.h>
+#endif
 
 #define LED_PIN 17
 #define NUM_PINS 13
 
-void sendMidiOn(int note)
+void sendUSBMIDI(unsigned char event, int channel, unsigned char pitch, unsigned char velocity)
 {
-    int pitch = 60 + note;                                   // Middle C plus note number
-    midiEventPacket_t noteOn = {0x09, 0x90 | 0, pitch, 100}; // channel 1, calculated pitch, velocity 100
+#ifdef IS_PRO_MICRO
+    midiEventPacket_t noteOn = {event, channel, pitch, velocity}; // channel 1, calculated pitch, velocity 100
     MidiUSB.sendMIDI(noteOn);
     MidiUSB.flush();
+#endif
+}
+
+void sendSerialMidi(uint8_t header, uint8_t data1, uint8_t data2)
+{
+#ifdef IS_PRO_MICRO
+    auto &s = Serial1;
+#else
+    auto &s = Serial;
+#endif
 
     // Send note on command over serial port
-    Serial1.write(0x90);  // Note on command
-    Serial1.write(pitch); // Note number
-    Serial1.write(100);   // Velocity
+    s.write(header); // Note on / off command
+    s.write(data1);  // Note number
+    s.write(data2);  // Velocity
+    s.flush();
+}
+
+void sendMidiOn(int note)
+{
+    int pitch = 60 + note; // Middle C plus note number
+
+    sendSerialMidi(0x90, pitch, 100);
+    // sendUSBMIDI(0x09, 0x90 | 0, pitch, 100);
 }
 
 void sendMidiOff(int note)
 {
-    int pitch = 60 + note;                                  // Middle C plus note number
-    midiEventPacket_t noteOff = {0x08, 0x80 | 0, pitch, 0}; // channel 1, calculated pitch, velocity 0
-    MidiUSB.sendMIDI(noteOff);
-    MidiUSB.flush();
+    int pitch = 60 + note; // Middle C plus note number
 
-    // Send note off command over serial port
-    Serial1.write(0x80);  // Note off command
-    Serial1.write(pitch); // Note number
-    Serial1.write(0);     // Velocity
+    sendSerialMidi(0x80, pitch, 0);
+    // sendUSBMIDI(0x08, 0x80 | 0, pitch, 0);
 }
 
 struct PinState
@@ -52,13 +69,13 @@ struct PinState
         { // If pin state has changed
             if (newState == HIGH)
             {
-                sendMidiOff(note);
                 digitalWrite(LED_PIN, HIGH);
+                sendMidiOff(note);
             }
             else
             {
-                sendMidiOn(note);
                 digitalWrite(LED_PIN, LOW);
+                sendMidiOn(note);
             }
 
             oldState = newState; // Update old pin state
@@ -67,15 +84,36 @@ struct PinState
     }
 };
 
+#ifdef IS_PRO_MICRO
 // All pins used in the pro micro:
-// {4, 5, 6, 7, 8, 9, 10, 16, 14, 15, 18, 19, 20};
-
+//{4, 5, 6, 7, 8, 9, 10, 16, 14, 15, 18, 19, 20};
 PinState pinStates[NUM_PINS] = {4, 5, 6, 7, 8, 9, 10, 16, 14, 15, 18, 19, 20};
+int noteOffsets[13] = {2, 1, 0, 12, 3, 9, 10, 11, 8, 7, 6, 5, 4}; // The order the notes came out when just pushing the pins in order
+#else
+// TODO: Handle this for uno
+PinState pinStates[NUM_PINS] = {4, 5, 6, 7, 8, 9, 10, 16, 14, 15, 18, 19, 20};
+int noteOffsets[13] = {2, 1, 0, 12, 3, 9, 10, 11, 8, 7, 6, 5, 4}; // The order the notes came out when just pushing the pins in order
+#endif
+
+int correctedOffsets[13];
+
+void setupCorrectedOffsets()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        correctedOffsets[noteOffsets[i]] = i;
+    }
+}
 
 void setup()
 {
+    setupCorrectedOffsets();
+#ifdef IS_PRO_MICRO
     Serial.begin(9600);
     Serial1.begin(31250); // Begin MIDI communication on Serial1
+#else
+    Serial.begin(31250);                                          // Begin MIDI communication on Serial
+#endif
     pinMode(LED_PIN, OUTPUT);
 
     for (int i = 0; i < NUM_PINS; i++)
@@ -89,6 +127,6 @@ void loop()
     for (int i = 0; i < NUM_PINS; i++)
     {
         int pinState = digitalRead(pinStates[i].pin);
-        pinStates[i].handleValue(pinState, i);
+        pinStates[i].handleValue(pinState, correctedOffsets[i]);
     }
 }
